8cd3fdb5f6f1ef7b0ab1fec6e33d18ac8c741d4b
Samir022 <119496487+Samir022@users.noreply.github.com>
2022-12-14 12:51:04 +0100

    updatet

--- java.js
+++ java.js
@@ -1,34 +1,3 @@
-;
-
-/**
- * element toggle function
- */
-
-const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
-
-/**
- * navbar variables
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
-const overlay = document.querySelector("[data-overlay]");
-
-const navElemArr = [navToggleBtn, overlay];
-
-for (let i = 0; i < navElemArr.length; i++) {
-
-  navElemArr[i].addEventListener("click", function () {
-    elemToggleFunc(navbar);
-    elemToggleFunc(overlay);
-  });
-
-}
-
-
-
 /**
  * header sticky
  */
@@ -48,38 +17,5 @@ window.addEventListener("scroll", function () {
   }
 
   lastScrollPosition = scrollPosition <= 0 ? 0 : scrollPosition;
-
-});
-
-
-
-/**
- * form validation
- */
-
-const input = document.querySelector("[data-input]");
-const submitBtn = document.querySelector("[data-submit]");
-
-input.addEventListener("input", function () {
-
-  if (input.checkValidity()) {
-    submitBtn.removeAttribute("disabled");
-  } else {
-    submitBtn.setAttribute("disabled", "");
-  }
-
-});
-
-
-
-/**
- * go top
- */
-
-const goTopBtn = document.querySelector("[data-go-top]");
-
-window.addEventListener("scroll", function () {
-
-  window.scrollY >= 200 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 });
